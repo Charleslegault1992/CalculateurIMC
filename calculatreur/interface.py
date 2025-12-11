3944ab9626a81070f72a9fb872d99d54fd716c53
Francis Boisvert <francisbvrt@gmail.com>
2025-12-11 16:34:05 -0500

    feat: Ajout de la fonction demander_infos

--- calculatreur/interface.py
+++ calculatreur/interface.py
@@ -5,7 +5,20 @@
 # Faire des rebases pour garder l'historique propre
 
 def demander_infos():
+    poids = 0
+    while True:
+        try:
+            if poids == 0:
+                poids = int(input("Entrez votre poids (en kg.): "))                
+            taille = int(input("Entrez votre taille (en cm.): "))
+            return poids, taille
+        except ValueError:
+            print("Cette entrée est invalide, veuillez entrer seulement des nombres")
     # Demande poids et taille
 
 def afficher_resultat(imc, categorie):
+    pass
     # Affiche joliment
+
+
+demander_infos()
