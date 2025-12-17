29e795bc98d0e5cab7be919b06fbc070fa67e58b
toleen-m <msabeetl@gmail.com>
2025-12-17 14:25:58 -0500

    fix: feature historique

--- calculatreur/historique.py
+++ calculatreur/historique.py
@@ -6,13 +6,21 @@
 
 def sauvegarder_calcul(nom, imc):
     # Sauvegarde dans un fichier
+    # cree un fichier et ajout les informations dans ce fichier
     with open("imc_historique.txt", "r", encoding="utf-8") as fichier:
         fichier.write(f"{nom}: {imc}\n")
 
 def afficher_historique():
     # Lit le fichier
-    while True:
+    try:
         with open("imc_historique.txt", "r", encoding="utf-8") as fichier:
             continu = fichier.read()
-            print("Historique des calculs IMC :")
-            print(continu)
+            if continu:
+                print("Historique des calculs IMC :")
+                print(continu)
+            # si le fichier est vide
+            else:
+                print("Aucun historique disponible")
+    # si le fichier nas pas ete cree
+    except FileNotFoundError:
+        print("Aucun historique disponible")
