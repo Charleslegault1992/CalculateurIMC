ef0971717495b428f3e6f2e63f6024718169b7a4
toleen-m <msabeetl@gmail.com>
2025-12-17 13:03:42 -0500

    feature: historique

--- calculatreur/historique.py
+++ calculatreur/historique.py
@@ -6,6 +6,13 @@
 
 def sauvegarder_calcul(nom, imc):
     # Sauvegarde dans un fichier
+    with open("imc_historique.txt", "r", encoding="utf-8") as fichier:
+        fichier.write(f"{nom}: {imc}\n")
 
 def afficher_historique():
     # Lit le fichier
+    while True:
+        with open("imc_historique.txt", "r", encoding="utf-8") as fichier:
+            continu = fichier.read()
+            print("Historique des calculs IMC :")
+            print(continu)
